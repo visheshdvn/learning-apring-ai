22f8033584f4385956a3d1790186c8a6a210212b
visheshdvn <vishesh.dh1@gmail.com>
2026-03-09 01:28:22 +0530

    chatClient bean creation separated from controller

---
+++ src/main/java/com/eazybytes/openai/config/ChatClientConfig.java
@@ -0,0 +1,26 @@
+package com.eazybytes.openai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig
+{
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder)
+    {
+
+        return chatClientBuilder
+                .defaultSystem("""
+                                       You are an internal HR assistant. Your role is to help\s
+                                       employees with questions related to HR policies, such as\s
+                                       leave policies, working hours, benefits, and code of conduct.
+                                       If a user asks for help with anything outside of these topics,\s
+                                       kindly inform them that you can only assist with queries related to\s
+                                       HR policies.
+                                       """)
+                .defaultUser("How can you help me?")
+                .build();
+    }
+}
