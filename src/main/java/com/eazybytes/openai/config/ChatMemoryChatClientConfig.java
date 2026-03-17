78b177bde8566f930985554fa037e997a8ab27a1
visheshdvn <vishesh.dh1@gmail.com>
2026-03-18 00:42:28 +0530

    chat memory with JdbcChatMemory implemented

---
+++ src/main/java/com/eazybytes/openai/config/ChatMemoryChatClientConfig.java
@@ -0,0 +1,32 @@
+package com.eazybytes.openai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatMemoryChatClientConfig
+{
+
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository)
+    {
+        return MessageWindowChatMemory.builder().maxMessages(10).chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
+
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory)
+    {
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return chatClientBuilder.defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor)).build();
+    }
+}
