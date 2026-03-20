12a200eea9ac9e61c35ea755433495a9692a39cd
visheshdvn <vishesh.dh1@gmail.com>
2026-03-21 01:22:50 +0530

    RAG: docs splitted to reduce tokens consumption

--- src/main/java/com/eazybytes/openai/rag/HRPolicyLoader.java
+++ src/main/java/com/eazybytes/openai/rag/HRPolicyLoader.java
@@ -3,6 +3,8 @@ package com.eazybytes.openai.rag;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -28,6 +30,9 @@ public class HRPolicyLoader
     {
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(policyFile);
         List<Document> docs = tikaDocumentReader.get();
-        vectorStore.add(docs);
+
+        TextSplitter textSplitter = TokenTextSplitter.builder().withChunkSize(100).withMaxNumChunks(400).build();
+
+        vectorStore.add(textSplitter.split(docs));
     }
 }
