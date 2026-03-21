f35855f518061605f15f066637243d09219b9469
visheshdvn <vishesh.dh1@gmail.com>
2026-03-22 02:46:36 +0530

    RAG implemented using RetrievalAugmentationAdvisor

--- src/main/java/com/eazybytes/openai/config/ChatMemoryChatClientConfig.java
+++ src/main/java/com/eazybytes/openai/config/ChatMemoryChatClientConfig.java
@@ -8,6 +8,9 @@ import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,13 +26,44 @@ public class ChatMemoryChatClientConfig
         return MessageWindowChatMemory.builder().maxMessages(10).chatMemoryRepository(jdbcChatMemoryRepository).build();
     }
 
+    /*
+    NOTE: Below text is added by the Retrieval Augmentation Advisor as a system prompt after the context information -
+
+    -----
+    Given the context information and no prior knowledge, answer the query.
+
+    Follow these rules:
+
+    1. If the answer is not in the context, just say that you don't know.
+    2. Avoid statements like "Based on the context..." or "The provided information...".
+
+    Query: <user's query>
+    -----
+     */
+    @Bean
+    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore)
+    {
+        return RetrievalAugmentationAdvisor.builder().documentRetriever(
+                VectorStoreDocumentRetriever.builder().vectorStore(vectorStore).topK(3).similarityThreshold(0.5).build()
+        ).build();
+    }
+
     @Bean("chatMemoryChatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory)
+    public ChatClient chatClient(
+            ChatClient.Builder chatClientBuilder,
+            ChatMemory chatMemory,
+            RetrievalAugmentationAdvisor retrievalAugmentationAdvisor
+    )
     {
         Advisor loggerAdvisor = new SimpleLoggerAdvisor();
         Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
         Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
 
-        return chatClientBuilder.defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor, tokenUsageAdvisor)).build();
+        return chatClientBuilder.defaultAdvisors(List.of(
+                loggerAdvisor,
+                memoryAdvisor,
+                tokenUsageAdvisor,
+                retrievalAugmentationAdvisor
+        )).build();
     }
 }
--- src/main/java/com/eazybytes/openai/controller/RAGController.java
+++ src/main/java/com/eazybytes/openai/controller/RAGController.java
@@ -1,17 +1,12 @@
 package com.eazybytes.openai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.document.Document;
-import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
@@ -40,18 +35,18 @@ public class RAGController
             @RequestParam("message") String message
     )
     {
-        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
-
-        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
-
-        String similarContext = similarDocs.stream()
-                                           .map(Document::getText)
-                                           .collect(Collectors.joining(System.lineSeparator()));
+//        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+//
+//        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+//
+//        String similarContext = similarDocs.stream()
+//                                           .map(Document::getText)
+//                                           .collect(Collectors.joining(System.lineSeparator()));
 
 
         String answer = chatClient.prompt()
-                                  .system(promptSystemSpec -> promptSystemSpec.text(promptTemplate)
-                                                                              .param("documents", similarContext))
+//                                  .system(promptSystemSpec -> promptSystemSpec.text(promptTemplate)
+//                                                                              .param("documents", similarContext))
                                   .advisors(a -> a.param(CONVERSATION_ID, username))
                                   .user(message)
                                   .call()
@@ -66,18 +61,18 @@ public class RAGController
             @RequestParam("message") String message
     )
     {
-        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
-
-        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
-
-        String similarContext = similarDocs.stream()
-                                           .map(Document::getText)
-                                           .collect(Collectors.joining(System.lineSeparator()));
+//        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+//
+//        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+//
+//        String similarContext = similarDocs.stream()
+//                                           .map(Document::getText)
+//                                           .collect(Collectors.joining(System.lineSeparator()));
 
 
         String answer = chatClient.prompt()
-                                  .system(promptSystemSpec -> promptSystemSpec.text(hrSystemTemplate)
-                                                                              .param("documents", similarContext))
+//                                  .system(promptSystemSpec -> promptSystemSpec.text(hrSystemTemplate)
+//                                                                              .param("documents", similarContext))
                                   .advisors(a -> a.param(CONVERSATION_ID, username))
                                   .user(message)
                                   .call()
