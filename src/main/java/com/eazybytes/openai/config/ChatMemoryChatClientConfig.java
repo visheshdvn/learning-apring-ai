732b37109f8a7e5ac53d473ebdc6f77c150cd9ba
visheshdvn <vishesh.dh1@gmail.com>
2026-03-24 23:12:26 +0530

    language translation implemented (concept covered: Pre-retrieval)

--- src/main/java/com/eazybytes/openai/config/ChatMemoryChatClientConfig.java
+++ src/main/java/com/eazybytes/openai/config/ChatMemoryChatClientConfig.java
@@ -9,6 +9,7 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
@@ -20,50 +21,63 @@ import java.util.List;
 public class ChatMemoryChatClientConfig
 {
 
-    @Bean
-    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository)
-    {
-        return MessageWindowChatMemory.builder().maxMessages(10).chatMemoryRepository(jdbcChatMemoryRepository).build();
-    }
+	@Bean
+	ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository)
+	{
+		return MessageWindowChatMemory.builder().maxMessages(10).chatMemoryRepository(jdbcChatMemoryRepository).build();
+	}
 
-    /*
-    NOTE: Below text is added by the Retrieval Augmentation Advisor as a system prompt after the context information -
+	/*
+	NOTE: Below text is added by the Retrieval Augmentation Advisor as a system prompt after the context information -
 
-    -----
-    Given the context information and no prior knowledge, answer the query.
+	-----
+	Given the context information and no prior knowledge, answer the query.
 
-    Follow these rules:
+	Follow these rules:
 
-    1. If the answer is not in the context, just say that you don't know.
-    2. Avoid statements like "Based on the context..." or "The provided information...".
+	1. If the answer is not in the context, just say that you don't know.
+	2. Avoid statements like "Based on the context..." or "The provided information...".
 
-    Query: <user's query>
-    -----
-     */
-    @Bean
-    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore)
-    {
-        return RetrievalAugmentationAdvisor.builder().documentRetriever(
-                VectorStoreDocumentRetriever.builder().vectorStore(vectorStore).topK(3).similarityThreshold(0.5).build()
-        ).build();
-    }
+	Query: <user's query>
+	-----
+	 */
+	@Bean
+	RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(
+			VectorStore vectorStore,
+			ChatClient.Builder chatClientBuilder
+	)
+	{
+		return RetrievalAugmentationAdvisor.builder()
+										   .queryTransformers(TranslationQueryTransformer.builder()
+																						 .chatClientBuilder(
+																								 chatClientBuilder.clone())
+																						 .targetLanguage("english")
+																						 .build())
+										   .documentRetriever(
+												   VectorStoreDocumentRetriever.builder()
+																			   .vectorStore(vectorStore)
+																			   .topK(3)
+																			   .similarityThreshold(0.5)
+																			   .build()
+										   ).build();
+	}
 
-    @Bean("chatMemoryChatClient")
-    public ChatClient chatClient(
-            ChatClient.Builder chatClientBuilder,
-            ChatMemory chatMemory,
-            RetrievalAugmentationAdvisor retrievalAugmentationAdvisor
-    )
-    {
-        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
-        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
-        Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+	@Bean("chatMemoryChatClient")
+	public ChatClient chatClient(
+			ChatClient.Builder chatClientBuilder,
+			ChatMemory chatMemory,
+			RetrievalAugmentationAdvisor retrievalAugmentationAdvisor
+	)
+	{
+		Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+		Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+		Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
 
-        return chatClientBuilder.defaultAdvisors(List.of(
-                loggerAdvisor,
-                memoryAdvisor,
-                tokenUsageAdvisor,
-                retrievalAugmentationAdvisor
-        )).build();
-    }
+		return chatClientBuilder.defaultAdvisors(List.of(
+				loggerAdvisor,
+				memoryAdvisor,
+				tokenUsageAdvisor,
+				retrievalAugmentationAdvisor
+		)).build();
+	}
 }
