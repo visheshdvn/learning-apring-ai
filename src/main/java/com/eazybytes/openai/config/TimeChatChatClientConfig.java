585ccbd909011a6f1d0b5f91412502bb3aec16f7
visheshdvn <vishesh.dh1@gmail.com>
2026-03-25 18:55:58 +0530

    time tools created to interact with llm

---
+++ src/main/java/com/eazybytes/openai/config/TimeChatChatClientConfig.java
@@ -0,0 +1,38 @@
+package com.eazybytes.openai.config;
+
+import com.eazybytes.openai.advisors.TokenUsageAuditAdvisor;
+import com.eazybytes.openai.tools.TimeTools;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class TimeChatChatClientConfig
+{
+
+	@Bean("timeChatClient")
+	public ChatClient chatClient(
+			ChatClient.Builder chatClientBuilder,
+			ChatMemory chatMemory,
+			TimeTools timeTools
+	)
+	{
+		Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+		Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+		Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+
+		return chatClientBuilder
+				.defaultTools(timeTools)
+				.defaultAdvisors(List.of(
+				loggerAdvisor,
+				memoryAdvisor,
+				tokenUsageAdvisor
+		)).build();
+	}
+}
