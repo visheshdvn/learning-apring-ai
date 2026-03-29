e212349e3f506f22d799ba4970bb7a1954fa67a2
visheshdvn <vishesh.dh1@gmail.com>
2026-03-29 20:50:45 +0530

    mcp client project initiated

---
+++ mcpclient/src/main/java/com/eazybytes/mcpclient/McpclientApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.mcpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpclientApplication.class, args);
+	}
+
+}
---
+++ mcpclient/src/test/java/com/eazybytes/mcpclient/McpclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.mcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
